f6228a176b0718d0d4af22f554585a0f5c0d4bc9
Dariel Amores <dariel901231@gmail.com>
2023-01-18 16:22:35 -0500

    Add PostLookupResponse to return the list of post for each type of query. Also, a new controller was added with five endpoints to take care for the requests.

--- CQRS-ES/CQRS.Core/Infrastructure/IQueryDispatcher.cs
+++ CQRS-ES/CQRS.Core/Infrastructure/IQueryDispatcher.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CQRS.Core.Queries;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace CQRS.Core.Infrastructure
 {
-    public interface IQueryDispatcher
+    public interface IQueryDispatcher<TEntity>
     {
+        void RegisterHanlder<TQuery>(Func<TQuery, Task<List<TEntity>>> handler) where TQuery : BaseQuery;
+
+        Task<List<TEntity>> SendAsync(BaseQuery query);
     }
 }
---
+++ SM-Post/Post.Query/Post.Query.Api/Controllers/PostLookupController.cs
@@ -0,0 +1,120 @@
+﻿using CQRS.Core.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Post.Common.DTOs;
+using Post.Query.Api.DTOs;
+using Post.Query.Api.Queries;
+using Post.Query.Domain.Entities;
+
+namespace Post.Query.Api.Controllers;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class PostLookupController : ControllerBase
+{
+    private readonly IQueryDispatcher<PostEntity> queryDispatcher;
+    private readonly ILogger<PostLookupController> logger;
+
+    public PostLookupController(IQueryDispatcher<PostEntity> queryDispatcher, ILogger<PostLookupController> logger)
+    {
+        this.queryDispatcher = queryDispatcher;
+        this.logger = logger;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> GetAllPostsAsync()
+    {
+        try
+        {
+            var posts = await this.queryDispatcher.SendAsync(new FindAllPostsQuery());
+            return NormalResponse(posts);
+        }
+        catch (Exception ex)
+        {
+            const string SAFE_ERROR_MESSAGE = "Error while processing request to retrieve all posts!";
+            return ErrorResponse(ex, SAFE_ERROR_MESSAGE);
+        }
+    }
+
+    [HttpGet("byId/{postId}")]
+    public async Task<ActionResult> GetPostIdAsync(Guid id)
+    {
+        try
+        {
+            var posts = await this.queryDispatcher.SendAsync(new FindPostByIdQuery { Id = id });
+            return NormalResponse(posts);
+        }
+        catch (Exception ex)
+        {
+            const string SAFE_ERROR_MESSAGE = "Error while processing request to find the post by ID!";
+            return ErrorResponse(ex, SAFE_ERROR_MESSAGE);
+        }
+    }
+
+    [HttpGet("byAuthor/{author}")]
+    public async Task<ActionResult> GetPostsByAuthorAsync(string author)
+    {
+        try
+        {
+            var posts = await this.queryDispatcher.SendAsync(new FindPostsByAuthorQuery { Author = author });
+            return NormalResponse(posts);
+        }
+        catch (Exception ex)
+        {
+            const string SAFE_ERROR_MESSAGE = "Error while processing request to find the posts by author!";
+            return ErrorResponse(ex, SAFE_ERROR_MESSAGE);
+        }
+    }
+
+    [HttpGet("withComments")]
+    public async Task<ActionResult> GetPostsWithCommentsAsync()
+    {
+        try
+        {
+            var posts = await this.queryDispatcher.SendAsync(new FindPostsWithCommentsQuery());
+            return NormalResponse(posts);
+        }
+        catch (Exception ex)
+        {
+            const string SAFE_ERROR_MESSAGE = "Error while processing request to find the posts with comments!";
+            return ErrorResponse(ex, SAFE_ERROR_MESSAGE);
+        }
+    }
+
+    [HttpGet("withLikes/{numberOfLikes}")]
+    public async Task<ActionResult> GetPostsWithLikesAsync(int numberOfLikes)
+    {
+        try
+        {
+            var posts = await this.queryDispatcher.SendAsync(new FindPostsWithLikesQuery { NumberOfLikes = numberOfLikes });
+            return NormalResponse(posts);
+        }
+        catch (Exception ex)
+        {
+            const string SAFE_ERROR_MESSAGE = "Error while processing request to find the posts with likes!";
+            return ErrorResponse(ex, SAFE_ERROR_MESSAGE);
+        }
+    }
+
+    private ActionResult ErrorResponse(Exception ex, string safeErrorMessage)
+    {
+        this.logger.LogError(ex, safeErrorMessage);
+
+        return StatusCode(StatusCodes.Status500InternalServerError, new BaseResponse
+        {
+            Message = safeErrorMessage,
+        });
+    }
+
+    private ActionResult NormalResponse(List<PostEntity> posts)
+    {
+        if (posts == null || !posts.Any())
+            return NoContent();
+        var count = posts.Count();
+        return Ok(new PostLookupResponse
+        {
+            Posts = posts,
+            Message = $"Successfully returned {count} post {(count > 1 ? "s" : string.Empty)}!"
+        });
+    }
+}
